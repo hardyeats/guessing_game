c05f4959f6d071c7d4f65b111652defe44755b79
hardyeats <hardyeats702@gmail.com>
2018-05-11 18:37:17 +0900

    주석 추가

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    // let : 변수를 만들 때
+    // let : 변수를 만들 때 사용
+    // mut : mutable. 러스트의 변수는 기본적으로 immutable.
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)

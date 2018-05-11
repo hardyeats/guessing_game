0a45bca01d021d3f5ce8e25672e9eda364ec30d5
hardyeats <hardyeats702@gmail.com>
2018-05-11 18:27:49 +0900

    input user's number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    // let : 변수를 만들 때
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
